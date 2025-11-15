0304bddd9df2ecf0ef088fb9227048d773c12cf8
Markku Sukanen <markku.sukanen@gmail.com>
2025-11-16 00:44:50 +0200

    Zone - generally orbital zones, but can be used for anything zone'ish.

---
+++ src/unit/zone.rs
@@ -0,0 +1,42 @@
+use std::ops::RangeInclusive;
+
+use serde::{Deserialize, Serialize};
+
+use crate::{celestial::orbital::OrbitalEccentricity, unit::{AsMetric, Metric}};
+
+pub enum ZoneDead {
+    Z1,
+    Z2,
+    Both
+}
+
+/// A generic "zone"…
+#[derive(Debug, Deserialize, Serialize, Clone)]
+pub enum Zone {
+    /// A "no limits" zone.
+    Free,
+    /// A limited zone with inner/outer limits.
+    Limited { inner: Metric, outer: Metric }
+}
+
+impl From<&OrbitalEccentricity> for Zone {
+    fn from(ecc: &OrbitalEccentricity) -> Self {
+        let inner = ecc.min_distance() / 3;
+        let outer = ecc.max_distance() * 3;
+        Self::Limited { inner, outer }
+    }
+}
+
+impl Zone {
+    pub fn adjust_limits(_z1: &mut Zone, _z2: &mut Zone) -> Result<(), ZoneDead> {
+        log::warn!("TODO: adjust_limits() - stub.");
+        Ok(())
+    }
+
+    pub fn as_range(&self) -> RangeInclusive<Metric> {
+        match self {
+            Self::Free => 0.au()..=f64::MAX.au(),
+            Self::Limited { inner, outer } => inner.clone()..=outer.clone()
+        }
+    }
+}
