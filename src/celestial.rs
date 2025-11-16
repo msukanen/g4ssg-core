8863fc10e7bfd89d28fa2d69416b732bb91620d3
Markku Sukanen <markku.sukanen@gmail.com>
2025-11-16 11:49:41 +0200

    intro CountCelestialMajors trait for counting major celestial objects...

--- src/celestial.rs
+++ src/celestial.rs
@@ -1,3 +1,8 @@
 pub mod orbital;
 pub mod star;
 pub mod system_composition;
+
+pub trait CountCelestialMajors {
+    /// Get number of major celestial objects (stars, black holes, etc.).
+    fn num_major_celestials(&self) -> usize;
+}
