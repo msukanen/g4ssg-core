c182a6a3ca25163583c0388279e28b71ea74fdac
Markku Sukanen <markku.sukanen@gmail.com>
2025-11-17 12:29:34 +0200

    plug in ab's and terrestrials. Comment out GG 'til the struct exists.

--- src/celestial.rs
+++ src/celestial.rs
@@ -1,9 +1,11 @@
-mod gas_giant;
-pub use gas_giant::GasGiant;
+pub mod ab;
+pub mod gas_giant;
+//pub use gas_giant::GasGiant;
 pub(crate) use gas_giant::{GasGiantArrangement, random_gg_distance};
 pub mod orbital;
 pub mod star;
 pub mod system_composition;
+pub mod terrestrial;
 
 pub trait CountCelestialMajors {
     /// Get number of major celestial objects (stars, black holes, etc.).
